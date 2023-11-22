25636e720f8c20e2ae062593dd7f893d334e9c66
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2023-11-22 13:08:03 +0100

    Working on the budget amount

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 
 interface BudgetInterface {
+  id: number;
   BudgetName: string;
-  BudgetTime:Date;
+  BudgetTime: string;
   BudgetAmount: string;
-  BudgetCategory:string
   CreatedAt: Date;
   UpdatedAt: Date;
-
 }
+
 @Component({
   selector: 'app-budget',
   templateUrl: './budget.component.html',
@@ -18,34 +18,37 @@ export class BudgetComponent {
   public BudgetName: string = '';
   public BudgetTime: string = '';
   public BudgetAmount: string = '';
-  public BudgetCategory: string = '';
   public BudgetArray: BudgetInterface[] = [];
   public created_at: Date = new Date();
   public updated_at: Date = new Date();
 
-  ngOnInit(){
-    const BudgetDetails = localStorage["customerDetails"];
-    if (BudgetDetails) {
-        this.BudgetArray = JSON.parse(BudgetDetails) as BudgetInterface[];
+  // ...
+
+  ngOnInit() {
+    // Load data from localStorage
+    const localStorageData = localStorage.getItem("BudgetDetails");
+
+    if (localStorageData) {
+      this.BudgetArray = JSON.parse(localStorageData) as BudgetInterface[];
     } else {
-        this.BudgetArray = [];
+      this.BudgetArray = [];
     }
   }
-  addBudget(){
-    // if(){
 
-    // }else{
-      const contactObject:any = {
-        Budget_Name:this.BudgetName,
-        Budget_Time:this.BudgetTime,
-        Budget_Amount: this.BudgetAmount,
-        Budget_Category: this.BudgetCategory,
-        CreatedAt: this.created_at,
-        UpdatedAt: this.updated_at,
-    };  
-    this.BudgetArray.push(contactObject);
-    console.log(contactObject);
+  addBudget() {
+    const id = new Date().getTime();
+    const budgetObject: BudgetInterface = {
+      id,
+      BudgetName: this.BudgetName,
+      BudgetTime: this.BudgetTime,
+      BudgetAmount: this.BudgetAmount,
+      CreatedAt: this.created_at,
+      UpdatedAt: this.updated_at,
+    };
+    this.BudgetArray.push(budgetObject);
     localStorage.setItem("BudgetDetails", JSON.stringify(this.BudgetArray));
-    }
-  // }
+    this.BudgetName = '';
+    this.BudgetTime = '';
+    this.BudgetAmount = '';
+  }
 }
